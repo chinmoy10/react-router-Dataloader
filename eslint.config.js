bc6206b443a8ccbbee172c620d097a1738ed7a4f
chinmoy10 <chinmoypodder22@gmail.com>
2026-04-25 14:52:18 +0600

    loaderData Practice running

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals, { node } from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -15,6 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+      node: true,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Comment = ({comment}) => {
     const{id, name, email} = comment || {};
     return (
-        <div className="border-2 border-pink-900 p-5 overflow-x-auto">
+        <div className="border-2 border-pink-900 p-5 overflow-x-auto flex flex-col">
             <h1>Id: {id}</h1>
             <h1>Name: {name}</h1>
-            <p>Email: {email}</p>
+            <p className="flex-grow">Email: {email}</p>
             <div className="mt-5 rounded-3xl text-pink-800 bg-slate-200">
                 <Link to={`/comment/${id}`}>Comment Details</Link>
             </div>
